2ee6f817e073ca864eb21aa2c5eea3e488b3a68f
ifotn <rich@infrontofthenet.com>
2015-07-27 14:47:05 -0400

    End of class commit

---
+++ comp2007-lesson12-mon/admin/departments.aspx.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+//reference our entity framework models
+using comp2007_lesson12_mon.Models;
+using System.Web.ModelBinding;
+
+namespace comp2007_lesson12_mon
+{
+    public partial class departments : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //fill the grid
+            if (!IsPostBack)
+            {
+                GetDepartments();
+            }
+        }
+
+        protected void GetDepartments()
+        {
+
+            //connect using our connection string from web.config and EF context class
+            using (DefaultConnectionEF conn = new DefaultConnectionEF())
+            {
+                //use link to query the Departments model
+                var deps = from d in conn.Departments
+                           select d;
+
+                //bind the query result to the gridview
+                grdDepartments.DataSource = deps.ToList();
+                grdDepartments.DataBind();
+                           
+            }
+        }
+
+        protected void grdDepartments_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            //connect
+            using (DefaultConnectionEF conn = new DefaultConnectionEF())
+            {
+                //get the selected DepartmentID
+                Int32 DepartmentID = Convert.ToInt32(grdDepartments.DataKeys[e.RowIndex].Values["DepartmentID"]);
+
+                var d = (from dep in conn.Departments
+                         where dep.DepartmentID == DepartmentID
+                         select dep).FirstOrDefault();
+
+                //process the delete
+                conn.Departments.Remove(d);
+                conn.SaveChanges();
+
+                //update the grid
+                GetDepartments();
+            }
+        }
+    }
+}
